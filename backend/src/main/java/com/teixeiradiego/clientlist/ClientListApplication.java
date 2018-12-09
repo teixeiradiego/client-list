0a1021497d5c8a703514d88d5fe6570b0cbaad81
Diego Nogueira Teixeira <frangocomp@gmail.com>
2018-12-09 14:39:20 -0200

    Created backend project

---
+++ backend/src/main/java/com/teixeiradiego/clientlist/ClientListApplication.java
@@ -0,0 +1,12 @@
+package com.teixeiradiego.clientlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientListApplication.class, args);
+	}
+}
